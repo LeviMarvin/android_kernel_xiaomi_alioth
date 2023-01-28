308b8275a3bd9bbe52eaf2d1e9fd5f4f8b10dd47
Levi Marvin <levimarvin@icloud.com>
2023-01-28 17:09:01 +0800

    lmperf: Update scripts.

--- lmperf/build.sh
+++ lmperf/build.sh
@@ -5,7 +5,13 @@ export KERNEL_ROOTDIR=`pwd`
 export KERNEL_OUTDIR=${KERNEL_ROOTDIR}/out
 target=alioth_lmperf_defconfig
 
+echo "Start building..."
 cd ${KERNEL_ROOTDIR}
 make ARCH=${ARCH} O=${KERNEL_OUTDIR} ARCH=${ARCH} SUBARCH=arm64 CC=clang LD=ld.lld ${target}
 cd ${KERNEL_OUTDIR}
 make -j12 O=${KERNEL_OUTDIR} ARCH=${ARCH} SUBARCH=arm64 CC=clang LD=ld.lld CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+cd ${KERNEL_ROOTDIR}
+echo "Packaging..."
+./lmperf/package.sh
+
---
+++ lmperf/package.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export KERNEL_ROOTDIR=`pwd`
+export KERNEL_OUTDIR=${KERNEL_ROOTDIR}/out
+export ANYKERNEL3_ROOTDIR=${KERNEL_ROOTDIR}/lmperf/AnyKernel3
+PRODUCT_OUTDIR=${KERNEL_ROOTDIR}/lmperf/out
+
+echo "Clean the output dir..."
+cd ${PRODUCT_OUTDIR}
+rm -rf ./*
+
+echo "Copy files..."
+cd ${ANYKERNEL3_ROOTDIR}
+cp ${KERNEL_OUTDIR}/arch/arm64/boot/Image ./
+
+echo "Set permissions..."
+chmod +x Image
+
+echo "Compress..."
+zip -r lmperf.zip anykernel.sh Image META-INF modules patch ramdisk tools
+cp lmperf.zip ${PRODUCT_OUTDIR}/
+
+echo "Clean the AnyKernel3 work dir..."
+rm -rf ${ANYKERNEL3_ROOTDIR}/Image ${ANYKERNEL3_ROOTDIR}/lmperf.zip
+
+echo "Package done!"
+echo "You can copy the file at \"${KERNEL_ROOTDIR}/lmperf/out/\""
