107125d6b21a4ca82cb5f1c8886cb455d569093e
Levi Marvin <levimarvin@icloud.com>
2023-03-11 17:26:47 +0800

    lmperf: modify build user and host information in release version

--- lmperf/build.sh
+++ lmperf/build.sh
@@ -2,17 +2,17 @@
 
 export ARCH=arm64
 export DTC_EXT=/usr/bin/dtc
-#export KBUILD_BUILD_USER=levimarvin
-#export KBUILD_BUILD_HOST=lmsite.eu.org
+export KBUILD_BUILD_USER=lmperf
+export KBUILD_BUILD_HOST=localhost
 export KERNEL_ROOTDIR=`pwd`
 export KERNEL_OUTDIR=${KERNEL_ROOTDIR}/out
 target=alioth_lmperf_defconfig
 
 echo "Start building..."
 cd ${KERNEL_ROOTDIR}
-make LLVM=1 LLVM_IAS=1 ARCH=${ARCH} O=${KERNEL_OUTDIR} ARCH=${ARCH} SUBARCH=arm64 CC=clang LD=ld.lld ${target}
+make LLVM=1 LLVM_IAS=1 O=${KERNEL_OUTDIR} ARCH=${ARCH} SUBARCH=arm64 CC=clang LD=ld.lld ${target}
 cd ${KERNEL_OUTDIR}
-make -j$(nproc) LLVM=1 LLVM_IAS=1 O=${KERNEL_OUTDIR} ARCH=${ARCH} SUBARCH=arm64 CC=clang LD=ld.lld CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+make -j$(nproc) LLVM=1 LLVM_IAS=1 O=${KERNEL_OUTDIR} ARCH=${ARCH} SUBARCH=arm64 CC=clang LD=ld.lld CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- KBUILD_BUILD_USER=${KBUILD_BUILD_USER} KBUILD_BUILD_HOST=${KBUILD_BUILD_HOST}
 
 echo "Packaging..."
 cd ${KERNEL_ROOTDIR}
